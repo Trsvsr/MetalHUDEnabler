7993250fbf8a127f1de9045fe45f6723974168f0
Trsvsr <trevor@schmitt.sh>
2023-03-29 23:35:30 -0500

    turns out I can utilize user defaults, how neat

--- MetalHUDEnabler/main.m
+++ MetalHUDEnabler/main.m
@@ -7,10 +7,12 @@
 
 #import <Foundation/Foundation.h>
 #import <sys/stat.h>
+#import <spawn.h>
 
+extern char **environ;
 struct stat st = {0};
 
-NSString* getExecutablePath(const char *applicationPath) {
+NSString *getBundleIdentifier(const char *applicationPath) {
     NSString *pathAsNSString = [NSString stringWithUTF8String:applicationPath];
     NSString *infoPlistPath = [pathAsNSString stringByAppendingString:@"/Contents/Info.plist"];
     if (stat([infoPlistPath UTF8String], &st)) {
@@ -18,40 +20,28 @@ NSString* getExecutablePath(const char *applicationPath) {
         exit(1);
     }
     NSDictionary *infoPlist = [NSDictionary dictionaryWithContentsOfFile:infoPlistPath];
-    NSString *exec = [infoPlist objectForKey:@"CFBundleExecutable"];
-    NSString *fullExecPath = [[pathAsNSString stringByAppendingString:@"/Contents/MacOS/"] stringByAppendingString:exec];
-    return fullExecPath;
+    NSString *bundleIdentifier = [infoPlist objectForKey:@"CFBundleIdentifier"];
+    return bundleIdentifier;
 }
 
-void enableMetalHUD(NSString *execPath) {
-    NSString *newExecPath = [execPath stringByAppendingString:@"_"];
-    if (stat([newExecPath UTF8String], &st)) {
-        printf("Renaming executable to %s\n", [newExecPath UTF8String]);
-        if (rename([execPath UTF8String], [newExecPath UTF8String])) {
-            printf("Failed to rename executable, aborting.\n");
-            exit(1);
-        }
-    }
-    FILE *file = fopen([execPath UTF8String], "w");
-    fprintf(file, "#!/bin/sh\nMTL_HUD_ENABLED=1 \"%s\"\n", [newExecPath UTF8String]);
-    fclose(file);
-    char* mode = "0755";
-    int octalMode = (int)strtol(mode, 0, 8);
-    chmod([execPath UTF8String], octalMode);
-    printf("Metal HUD should be enabled for %s\n", [execPath UTF8String]);
+void enableMetalHUD(NSString *bundleIdentifier) {
+    NSString *defaultsCommand = [NSString stringWithFormat:@"defaults write %@ MetalForceHudEnabled -bool true", bundleIdentifier];
+    pid_t pid;
+    int status;
+    const char *args[] = {"sh", "-c", [defaultsCommand UTF8String], NULL};
+    posix_spawn(&pid, "/bin/sh", NULL, NULL, (char *const *)args, environ);
+    waitpid(pid, &status, 0);
+    printf("Metal HUD should be enabled for %s\n", [bundleIdentifier UTF8String]);
 }
 
-void disableMetalHUD(NSString *execPath) {
-    NSString *newExecPath = [execPath stringByAppendingString:@"_"];
-    if (stat([newExecPath UTF8String], &st)) {
-        printf("Executable not found, aborting.\n");
-        exit(1);
-    }
-    if (rename([newExecPath UTF8String], [execPath UTF8String])) {
-        printf("Failed to rename executable, aborting.\n");
-        exit(1);
-    }
-    printf("Metal HUD should be disabled for %s\n", [execPath UTF8String]);
+void disableMetalHUD(NSString *bundleIdentifier) {
+    NSString *defaultsCommand = [NSString stringWithFormat:@"defaults delete %@ MetalForceHudEnabled", bundleIdentifier];
+    pid_t pid;
+    int status;
+    const char *args[] = {"sh", "-c", [defaultsCommand UTF8String], NULL};
+    posix_spawn(&pid, "/bin/sh", NULL, NULL, (char *const *)args, environ);
+    waitpid(pid, &status, 0);
+    printf("Metal HUD should be disabled for %s\n", [bundleIdentifier UTF8String]);
 }
 
 void printUsage() {
@@ -74,12 +64,12 @@ int main(int argc, const char * argv[]) {
             printUsage();
             return 0;
         }
-        NSString *execPath = getExecutablePath(argv[1]);
+        NSString *bundleIdentifier = getBundleIdentifier(argv[1]);
         if (!strcmp(argv[2], "enable")) {
-            enableMetalHUD(execPath);
+            enableMetalHUD(bundleIdentifier);
         }
         else if (!strcmp(argv[2], "disable")) {
-            disableMetalHUD(execPath);
+            disableMetalHUD(bundleIdentifier);
         }
         else {
             printf("Unknown argument\n");
